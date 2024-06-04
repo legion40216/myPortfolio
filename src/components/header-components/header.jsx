073d4ed28af77375e8f673ca0c8227dede8e110a
legion40216 <suleman_mughal@outlook.com>
2024-06-04 05:30:20 +0500

    added close funtionality when clicking on the menu options and click outside of body

--- src/components/header-components/header.jsx
+++ src/components/header-components/header.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './navbar-components/navbar'
 import { Menu, Sun } from 'lucide-react';
 
 export default function Header() {
   const [toggleNav, setToggleNav] = useState(false)
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (toggleNav && !event.target.closest('.nav-bar')) {
+        setToggleNav(false);
+      }
+    };
+
+    document.body.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.body.removeEventListener('click', handleOutsideClick);
+    };
+  }, [toggleNav]);
+
   return (
     <header>
       <div className='nav-bar' data-visible={toggleNav}>
-        <Navbar/>
+        <Navbar 
+        setToggleNav={setToggleNav}
+        />
           <div className='flex gap-2 flex-col md:flex-row'>
             <button className='p-2  rounded text-sm bg-teal-500'>
               Resume
@@ -19,17 +35,15 @@ export default function Header() {
           </div>
       </div>
 
-        <button 
-        onClick={()=>{setToggleNav(prvious => (!prvious))}}
-        className=' block md:hidden
-         p-2
-         rounded text-sm 
-        bg-blue-800 
-        hover:bg-slate-700 
-        '
-        >
-          <Menu className="h-5 w-5"/>
-        </button>
+      <button 
+        onClick={(e) => {
+          e.stopPropagation();
+          setToggleNav(prev => !prev);
+        }}
+        className=' block md:hidden p-2 rounded text-sm bg-blue-800 hover:bg-slate-700'
+      >
+        <Menu className="h-5 w-5"/>
+      </button>
     </header>
   )
 }
--- src/components/header-components/navbar-components/navbar.jsx
+++ src/components/header-components/navbar-components/navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-export default function Navbar() {
+export default function Navbar({
+  setToggleNav
+}) {
   return (
     <nav>
         <ul className="flex gap-5">
-            <li><NavLink to ="/">Home</NavLink></li>
-            <li><NavLink to ="project">Projects</NavLink></li>
-            <li><NavLink to ="about">About me</NavLink></li>
+            <li onClick={()=>{setToggleNav(prvious => (!prvious))}}><NavLink to ="/">Home</NavLink></li>
+            <li onClick={()=>{setToggleNav(prvious => (!prvious))}}><NavLink to ="project">Projects</NavLink></li>
+            <li onClick={()=>{setToggleNav(prvious => (!prvious))}}><NavLink to ="about">About me</NavLink></li>
         </ul>
     </nav>
   )
